c2903120664cf6157cd84282c7da7cca2c57b5fc
chelovevitsa <vita.bosatckaia30@gmail.com>
2023-06-06 12:04:40 +0200

    dlt

--- zawody-egzam/zgloszenie.php
+++
@@ -1,7 +0,0 @@
-<?php
-$conn = mysqli_connect('localhost', 'root', '', 'wedkarstwo');
-$zp1 = 'INSERT INTO `zawody_wedkarskie`(`Karty_wedkarskie_id`,`Lowisko_id`, `data_zawodow`, `sedzia`) VALUES (0."'$_POST['lowisko']'".","."'$_POST['data']'".","."'$_POST['sedzia']'")';
-$result = $conn -> query($zp1);
-
-$conn -> close();
-?>
